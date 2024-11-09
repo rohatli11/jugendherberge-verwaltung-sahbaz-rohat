f4d083597546122da3306c817f9f656ca21a7929
rohat.sahbaz@student.htl-rankweil.at <rohat.sahbaz@student.htl-rankweil.at>
2024-11-09 08:58:27 +0000

    Edited Startseite

--- client_code/Startseite/__init__.py
+++ client_code/Startseite/__init__.py
@@ -104,19 +104,19 @@ class Startseite(StartseiteTemplate):
             guest_name = guest_row.get_components()[1].text
 
             if guest_button.checked:
-                print(f"Gast ausgewählt: {guest_name}")
-                zimmer_id = anvil.server.call('get_zimmerid_from_zimmernummer', int(zimmer_num.text))
-                if zimmer_id:
-                  anvil.server.call('add_booking', start_date, end_date, preis, zimmer_id[0], self.benutzer_drop_down.selected_value, guest_name)
-                  print(f"Buchung hinzugefügt für Zimmernummer {zimmer_num.text} mit ID {zimmer_id[0]}")
-                  self.start_datum_picker.date = None  
-                  self.end_datum_picker.date = None  
-                  self.benutzer_drop_down.selected_value = 1
-                  self.jugendherberge_drop_down.selected_value = 1
-                  self.repeating_panel_1.items = []
-                  
-            else:
-              alert(f"Zimmer-ID für Zimmernummer {zimmer_num} konnte nicht gefunden werden.")
+              print(f"Gast ausgewählt: {guest_name}")
+              zimmer_id = anvil.server.call('get_zimmerid_from_zimmernummer', int(zimmer_num.text))
+              if zimmer_id:
+                anvil.server.call('add_booking', start_date, end_date, preis, zimmer_id[0], self.benutzer_drop_down.selected_value, guest_name)
+                print(f"Buchung hinzugefügt für Zimmernummer {zimmer_num.text} mit ID {zimmer_id[0]}")
+                self.start_datum_picker.date = None  
+                self.end_datum_picker.date = None  
+                self.benutzer_drop_down.selected_value = 1
+                self.jugendherberge_drop_down.selected_value = 1
+                self.repeating_panel_1.items = []  
+              else:
+                alert(f"Zimmer-ID für Zimmernummer {zimmer_num} konnte nicht gefunden werden.")
+            
 
     else:
         alert("Bitte füllen Sie alle erforderlichen Felder aus.", title="Fehlende Informationen")
